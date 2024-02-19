d547a773ec44f5ee7f76f47642322677c15a6d77
Yin jieyang <103308766+SeeYouCowboi@users.noreply.github.com>
2024-02-19 16:17:10 +0800

    Initial commit

---
+++ ModifiedMinsk/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace mc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">: ");
+            var line = Console.ReadLine(); ;
+            if (string.IsNullOrWhiteSpace(line))
+                return;
+        }
+    }
+}
